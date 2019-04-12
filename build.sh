7a571c7a2fe0d0272c4a657a278c15d7c3fc6125
Danny Lockard <danny.lockard@banno.com>
2019-04-12 11:12:05 -0500

    Adding diceroll to build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 
 go build -buildmode=plugin -o echo.so plugins/echo.go
+go build -buildmode=plugin -o diceroll.so plugins/diceroll.go
 go build
 
 echo "run \`SLACK_TOKEN="SLACK_TOKEN_HERE" ./OperatorBot\`"
